668e873f5516b017bf716510ca53190da9283a40
João Ricardo <joaoricardobr@aol.com>
2026-08-20 18:41:27 -0300

    Add metadata to LoginLayout component
    Added metadata for login layout including an icon.

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,5 +1,17 @@
+import type { Metadata } from 'next';
+
 export const dynamic = 'force-dynamic';
 
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  icons: {
+    icon: '/app/login/icon.svg',
+  },
+};
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return children;
 }
